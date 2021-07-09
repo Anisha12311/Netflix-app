78c4d15c10e652591d1af47bfe83e3df48e2fad9
Anisha Yadav <83083211+Anisha12311@users.noreply.github.com>
2021-07-09 16:44:00 +0530

    Add files via upload

---
+++ src/components/Banner.js
@@ -0,0 +1,55 @@
+
+import React,{useState,useEffect} from 'react'
+import axios from '../axios'
+import requests from '../requests'
+import './Banner.css'
+function Banner(){
+
+    const [movie,setMovie] = useState([])
+
+
+    useEffect(() =>{
+        async function fetchData() {
+            const request  = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length-1)
+                ]
+            )
+            return request ;
+        }
+
+        fetchData();
+    },[])
+
+    function truncate(str,n) {
+        return str?.length>n ? str.substr(0,n-1) + "...":str;
+    }
+
+    console.log(movie)
+    return (
+        <header className = "banner"
+            style = {{
+                backgroundSize : "cover",
+                backgroundImage :`url(
+                    "https://image.tmdb.org/t/p/original${movie?.backdrop_path}"
+                )`,
+                backgroundPostion : "center center"
+            }}>
+            <div className = "banner_contents">
+                <h1 className = "banner_title">
+                    {movie ?. title || movie ?.name || movie.original_name}
+                </h1>
+                <div className = "banner_buttons">
+                    <button className = "banner_button">Like</button>
+                    <button className = "banner_button">PlayList</button>
+                </div>
+              
+                <h1 class = "banner_description">{movie?.overview}</h1>
+                </div> 
+                < div className = "banner_fadder"/>
+        </header>
+    )
+}
+
+export default Banner
